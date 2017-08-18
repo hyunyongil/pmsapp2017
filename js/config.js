364794abe09e027bf09b944a4231afe08e482bac
yong <hyunyongil@naver.com>
2017-08-18 19:10:09 +0900

    kyle0818(10)

--- js/config.js
+++ js/config.js
@@ -2,12 +2,12 @@ APP_VERSION = '1.0.7';
 //CONNECTION_AJ_URL = 'http://loconsol.onsolutions.co.kr';//로컬
 //CONNECTION_URL = 'http://loconsol.onsolutions.co.kr';//로컬
 
-//CONNECTION_AJ_URL = 'http://devonsol.onsolutions.co.kr';//개발서버
-//CONNECTION_URL = 'http://devonsol.onsolutions.co.kr';//개발서버
+CONNECTION_AJ_URL = 'http://devonsol.onsolutions.co.kr';//개발서버
+CONNECTION_URL = 'http://devonsol.onsolutions.co.kr';//개발서버
 
 
-CONNECTION_AJ_URL = 'http://onsolutions.co.kr';//REAL서버
-CONNECTION_URL = 'http://page.onsolutions.co.kr';//REAL서버
+//CONNECTION_AJ_URL = 'http://onsolutions.co.kr';//REAL서버
+//CONNECTION_URL = 'http://page.onsolutions.co.kr';//REAL서버
 
 FILES_URL_DOMAIN = 'files.onsolutions.co.kr';
 FILES_URL = 'http://' + FILES_URL_DOMAIN;
