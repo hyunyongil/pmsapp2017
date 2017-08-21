ed9bd25c3909ee537a6bde1fc798b9fc687f46a1
yong <hyunyongil@naver.com>
2017-08-21 10:16:49 +0900

    kyle0821(1)

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@ APP_VERSION = '1.0.7';
 //CONNECTION_AJ_URL = 'http://loconsol.onsolutions.co.kr';//로컬
 //CONNECTION_URL = 'http://loconsol.onsolutions.co.kr';//로컬
 
-CONNECTION_AJ_URL = 'http://devonsol.onsolutions.co.kr';//개발서버
-CONNECTION_URL = 'http://devonsol.onsolutions.co.kr';//개발서버
+CONNECTION_AJ_URL = 'http://devonpanel.onsolutions.co.kr';//개발서버
+CONNECTION_URL = 'http://devonpanel.onsolutions.co.kr';//개발서버
 
 
 //CONNECTION_AJ_URL = 'http://onsolutions.co.kr';//REAL서버
